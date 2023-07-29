cdaf81f0de33bf4878aed0c9b2c5aed18a46a55a
zBilll <zzkiqzz@gmail.com>
2023-07-29 17:36:10 +0700

    edit Authen Page

---
+++ src/pages/AuthenticPage/RegisterPage.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import Register from "~/components/Form/Register";
+
+function RegisterPage() {
+  return(
+    <div>
+      <Register />
+    </div>
+  )
+}
+
+export default RegisterPage
